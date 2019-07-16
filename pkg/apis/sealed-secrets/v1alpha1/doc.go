5b3c1f5328a2604ad2896acab1e7a8bf2955c9f2
Marko Mikulicic <mkm@bitnami.com>
2019-07-16 10:42:55 +0200

    Ensure k8s.io/code-generator is vendored
    Closes #153

--- pkg/apis/sealed-secrets/v1alpha1/doc.go
+++ pkg/apis/sealed-secrets/v1alpha1/doc.go
@@ -1,4 +1,5 @@
-//go:generate ../../../../vendor/k8s.io/code-generator/generate-groups.sh all github.com/bitnami-labs/sealed-secrets/pkg/client github.com/bitnami-labs/sealed-secrets/pkg/apis sealed-secrets:v1alpha1
+// go mod vendor doesn't preserve executable perm bits
+//go:generate bash ../../../../vendor/k8s.io/code-generator/generate-groups.sh all github.com/bitnami-labs/sealed-secrets/pkg/client github.com/bitnami-labs/sealed-secrets/pkg/apis sealed-secrets:v1alpha1 --go-header-file boilerplate.go.txt
 // +k8s:deepcopy-gen=package,register
 
 // +groupName=bitnami.com
