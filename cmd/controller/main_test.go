4856bd5aff9c7d28a738be937361ab92b6dfda4c
Marko Mikulicic <mkm@bitnami.com>
2019-07-17 21:42:11 +0200

    Disable key rotation by default
    It's premature to enable key-rotation by default. See open issues like #185.
    Ref #137
    
    This should unblock the possibility of cutting a release with a lot of important features
    and let users who want key-rotation to opt-in and deal with the consequences.

--- cmd/controller/main_test.go
+++ cmd/controller/main_test.go
@@ -56,7 +56,7 @@ func TestInitKeyRotation(t *testing.T) {
 		t.Fatalf("initKeyRegistry() returned err: %v", err)
 	}
 
-	keyGenTrigger, err := initKeyRotation(registry, time.Hour)
+	keyGenTrigger, err := initKeyRotation(registry, 0)
 	if err != nil {
 		t.Fatalf("initKeyRotation() returned err: %v", err)
 	}
@@ -69,6 +69,39 @@ func TestInitKeyRotation(t *testing.T) {
 	// Test the trigger function
 	// Activates trigger and polls client every 50 ms up to 10s for the appropriate action
 	keyGenTrigger()
+	maxWait := 10 * time.Second
+	endTime := time.Now().Add(maxWait)
+	successful := false
+	for time.Now().Before(endTime) {
+		time.Sleep(50 * time.Millisecond)
+		if hasAction(client, "create", "secrets") {
+			successful = true
+			break
+		}
+	}
+	if !successful {
+		t.Errorf("trigger function failed to activate early key generation")
+	}
+}
+
+func TestInitKeyRotationTick(t *testing.T) {
+	rand := testRand()
+	client := fake.NewSimpleClientset()
+	registry, err := initKeyRegistry(client, rand, "namespace", "prefix", "label", 1024)
+	if err != nil {
+		t.Fatalf("initKeyRegistry() returned err: %v", err)
+	}
+
+	_, err = initKeyRotation(registry, 100*time.Millisecond)
+	if err != nil {
+		t.Fatalf("initKeyRotation() returned err: %v", err)
+	}
+	if !hasAction(client, "create", "secrets") {
+		t.Errorf("initKeyRotation() failed to generate an initial key")
+	}
+
+	client.ClearActions()
+
 	maxWait := 10 * time.Second
 	endTime := time.Now().Add(maxWait)
 	successful := false
