b21c58925216435a59e557542c84bc4e30424bfa
Marko Mikulicic <mkm@bitnami.com>
2019-07-19 16:15:39 +0200

    Generate new key only if needed
    Closes #190

--- cmd/controller/main_test.go
+++ cmd/controller/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
 	"testing"
 	"time"
 
@@ -114,5 +116,39 @@ func TestInitKeyRotationTick(t *testing.T) {
 	}
 	if !successful {
 		t.Errorf("trigger function failed to activate early key generation")
+	}
+}
+
+func TestReuseKey(t *testing.T) {
+	rand := testRand()
+	key, err := rsa.GenerateKey(rand, 512)
+	if err != nil {
+		t.Fatalf("Failed to generate test key: %v", err)
+	}
+
+	cert, err := signKey(rand, key)
+	if err != nil {
+		t.Fatalf("signKey failed: %v", err)
+	}
+
+	client := fake.NewSimpleClientset()
+	_, err = writeKey(client, key, []*x509.Certificate{cert}, "namespace", SealedSecretsKeyLabel, "prefix")
+	if err != nil {
+		t.Errorf("writeKey() failed with: %v", err)
+	}
+
+	client.ClearActions()
+
+	registry, err := initKeyRegistry(client, rand, "namespace", "prefix", SealedSecretsKeyLabel, 1024)
+	if err != nil {
+		t.Fatalf("initKeyRegistry() returned err: %v", err)
+	}
+
+	_, err = initKeyRotation(registry, 0)
+	if err != nil {
+		t.Fatalf("initKeyRotation() returned err: %v", err)
+	}
+	if hasAction(client, "create", "secrets") {
+		t.Errorf("initKeyRotation() should not create a new secret when one already exist and rotation is disabled")
 	}
 }
