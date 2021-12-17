9f38a6b76a79386bbf0b94a44ea14eb451bf2909
Mathieu Parent <mathieu.parent@insee.fr>
2021-12-17 11:03:50 +0100

    Fix Cannot fetch certificate: no endpoints available for service "http:sealed-secrets-controller:"
    See also https://istio.io/latest/docs/ops/configuration/traffic-management/protocol-selection/#explicit-protocol-selection
    
    Fixes: https://github.com/bitnami-labs/sealed-secrets/issues/502
    Fixes: https://github.com/bitnami-labs/sealed-secrets/issues/694

--- cmd/kubeseal/main.go
+++ cmd/kubeseal/main.go
@@ -211,7 +211,7 @@ func openCertURI(uri string) (io.ReadCloser, error) {
 func openCertCluster(c corev1.CoreV1Interface, namespace, name string) (io.ReadCloser, error) {
 	f, err := c.
 		Services(namespace).
-		ProxyGet("http", name, "", "/v1/cert.pem", nil).
+		ProxyGet("http", name, "http", "/v1/cert.pem", nil).
 		Stream()
 	if err != nil {
 		return nil, fmt.Errorf("cannot fetch certificate: %v", err)
