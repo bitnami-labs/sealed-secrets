4206f7619bb3d4112d110f5b0d2dbddbb7ebe9a5
devthejo <jo@surikat.pro>
2022-06-25 00:31:59 +0200

    feat: override rate limit

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -50,6 +50,8 @@ var (
 	namespaceAll         = flag.Bool("all-namespaces", true, "Scan all namespaces or only the current namespace (default=true).")
 	additionalNamespaces = flag.String("additional-namespaces", "", "Comma-separated list of additional namespaces to be scanned.")
 	labelSelector        = flag.String("label-selector", "", "Label selector which can be used to filter sealed secrets.")
+	rateLimitBurst       = flag.Int("rate-limit-burst", 2, "Number of requests per burst")
+	rateLimitDelay       = flag.Int("rate-limit-delay", 2, "Number of seconds to sleep between bursts")
 
 	oldGCBehavior = flag.Bool("old-gc-behaviour", false, "Revert to old GC behavior where the controller deletes secrets instead of delegating that to k8s itself.")
 
@@ -266,7 +268,7 @@ func main2() error {
 		return []*x509.Certificate{cert}, nil
 	}
 
-	server := httpserver(cp, controller.AttemptUnseal, controller.Rotate)
+	server := httpserver(cp, controller.AttemptUnseal, controller.Rotate, *rateLimitBurst, *rateLimitDelay)
 
 	sigterm := make(chan os.Signal, 1)
 	signal.Notify(sigterm, syscall.SIGTERM)
