d46fcdef83a780af68055d91a65874b960a32376
Jose Luis Vazquez Gonzalez <josvaz@vmware.com>
2022-09-27 15:37:29 +0200

    Use static matrices, but detect if they get stale

--- scripts/check-versions.sh
+++ scripts/check-versions.sh
@@ -9,21 +9,38 @@ VAR_COUNT=${#VARIABLES[@]}
 
 failures=0
 for ci_file in $(ls .github/workflows/*.y*ml); do
+  # Check variable name definitions
   defined_vars=$(grep '^env:.*$' -A "${VAR_COUNT}" "${ci_file}" || true)
   for var_name in $VARIABLES; do
     if echo "${defined_vars}" |grep -q "${var_name}"; then
       value=${!var_name}
       ci_value=$(grep '^env:.*$' -A "${VAR_COUNT}" "${ci_file}" |grep "${var_name}:" |awk '{print $2}')
       if [ "${value}" != "${ci_value}" ]; then
-        failures=$(( failures++ ))
+        ((failures+=1))
         echo "Fix ${ci_file} workflow environment variables to set ${var_name}=${value} (instead of ${ci_value})"
       fi
     fi
   done
+
+  # Check static matrices
+  expected_matrix_regex="go: \[\""${GO_VERSION}"\", \""${GO_NEXT_VERSION}"\"\]"
+  expected_matrix=$(echo "${expected_matrix_regex}" | sed -e 's/\\\[/[/' -e 's/\\\]/]/')
+  if grep 'matrix:' -A3 "${ci_file}" |grep -s 'go:' > /dev/null; then
+    matrices=($(IFS=$'\n'; grep 'matrix:' -A3 "${ci_file}" |grep 'go:' || true))
+    matrices_count=${#matrices[@]}
+    if (( $matrices_count > 0 )); then
+      for matrix in $matrices; do
+        if ! echo "${matrix}" | grep -q "${expected_matrix_regex}"; then
+          ((failures+=1))
+          echo "Fix ${ci_file} workflow matrix to be '${expected_matrix}' instead of '${matrix}'"
+        fi
+      done
+    fi
+  fi
 done
 
 if (( $failures > 0 )); then
-  echo "Found ${failures} version mistmatchs between CI settings and main version defintion file versions.env"
+  echo "Found ${failures} version mistmatchs between CI settings and main version definition file versions.env"
   exit 1 
 fi
-echo "OK"
+echo "OK failures=${failures}"
