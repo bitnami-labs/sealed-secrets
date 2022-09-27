da7533d4600eb40a1e0fc5a41725a46c26899cb3
Jose Luis Vazquez Gonzalez <josvaz@vmware.com>
2022-09-27 17:12:39 +0200

    Replace readarray by plain bash

--- scripts/check-versions.sh
+++ scripts/check-versions.sh
@@ -25,8 +25,12 @@ for ci_file in $(ls .github/workflows/*.y*ml); do
   # Check static matrices
   expected_matrix_regex="go: \[\""${GO_VERSION}"\", \""${GO_NEXT_VERSION}"\"\]"
   expected_matrix=$(echo "${expected_matrix_regex}" | sed -e 's/\\\[/[/' -e 's/\\\]/]/')
-  if grep 'matrix:' -A3 "${ci_file}" |grep -s 'go:' > /dev/null; then
-    readarray -t matrices < <(grep 'matrix:' -A3 "${ci_file}" | grep 'go:' | sed 's/^ *//')
+  raw_matrices=$(grep 'matrix:' -A3 "${ci_file}" | grep 'go:' | sed 's/^ *//' || true)
+  if [ "${raw_matrices}" != "" ]; then
+    matrices=()
+    while IFS='\n' read var value; do
+      matrices[${#matrices[@]}]=$var
+    done < <(echo "${raw_matrices}")
     for matrix in "${matrices[@]}"; do
       if ! echo "${matrix}" | grep -q "${expected_matrix_regex}"; then
         ((failures+=1))
@@ -40,4 +44,4 @@ if (( $failures > 0 )); then
   echo "Found ${failures} version mistmatchs between CI settings and main version definition file: versions.env"
   exit 1 
 fi
-echo "OK"
+echo "Versions OK"
