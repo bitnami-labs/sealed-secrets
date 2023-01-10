915e646ef22d8c67d222fff530f9d2484215c2e4
Alejandro Moreno <amorenoc@vmware.com>
2023-01-10 15:39:26 +0100

    Generate code
    Signed-off-by: Alejandro Moreno <amorenoc@vmware.com>

--- pkg/client/clientset/versioned/clientset.go
+++ pkg/client/clientset/versioned/clientset.go
@@ -17,8 +17,7 @@ type Interface interface {
 	BitnamiV1alpha1() bitnamiv1alpha1.BitnamiV1alpha1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	bitnamiV1alpha1 *bitnamiv1alpha1.BitnamiV1alpha1Client
