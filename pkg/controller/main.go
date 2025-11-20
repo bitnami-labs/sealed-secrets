987f10aa7706312e2f289f87c959208b27a95813
Levin Jeck <seternate@gmail.com>
2025-11-20 12:17:33 +0100

    Make controllers kubeclient QPS & Burst configurable. (#1834)
    **Description of the change**
    
    <!-- Describe the scope of your change - i.e. what the change does. -->
    
    The kubeclients QPS & Burst configuration was exposed by introducing
    additional CLI parameters. As well as exposing those parameters in the
    helm chart.
    
    **Benefits**
    
    When running SealedSecrets controller reconcilation speed at startup
    will be increased, because more requests can be made. Therefore new
    Secrets while reconcile should be decrypted faster when having thousand
    of Secrets in the cluster.
    
    **Applicable issues**
    
    - fixes #1833
    
    Signed-off-by: Levin Jeck <seternate@gmail.com>

--- pkg/controller/main.go
+++ pkg/controller/main.go
@@ -58,6 +58,8 @@ type Flags struct {
 	PrivateKeyLabels      string
 	MaxRetries            int
 	WatchForSecrets       bool
+	KubeClientQPS         float32
+	KubeClientBurst       int
 }
 
 func initKeyPrefix(keyPrefix string) (string, error) {
@@ -176,6 +178,9 @@ func Main(f *Flags, version string) error {
 	if err != nil {
 		return err
 	}
+
+	config.QPS = f.KubeClientQPS
+	config.Burst = f.KubeClientBurst
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
