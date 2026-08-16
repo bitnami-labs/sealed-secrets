a0fcd9b2d325345073c43b5a56a88d06e6bbc3f6
Andrew Melnick <meln5674@kettering.edu>
2026-08-16 17:28:21 -0400

    add imports to force vendoring required codegen tools
    Signed-off-by: Andrew Melnick <meln5674@kettering.edu>

--- hack/tools.go
+++ hack/tools.go
@@ -9,4 +9,8 @@ package tools
 
 import (
 	_ "k8s.io/code-generator"
+	_ "k8s.io/code-generator/cmd/conversion-gen"
+	_ "k8s.io/code-generator/cmd/deepcopy-gen"
+	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/code-generator/cmd/validation-gen"
 )
